4590899a18a71c769cc548fa9cec022f2613e4f5
Usuario <Usuario@CPD02-M03>
2024-08-20 12:26:53 -0300

    Adicionar arquivos de projeto.

---
+++ tEOREMAdOmACACOiNFINITO/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows.Forms;
+
+namespace tEOREMAdOmACACOiNFINITO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGERAR = new System.Windows.Forms.Button();
+            this.trkNIVEL = new System.Windows.Forms.TrackBar();
+            this.txtTEOREMA = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trkNIVEL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGERAR
+            // 
+            this.btnGERAR.Font = new System.Drawing.Font("Imprint MT Shadow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGERAR.Location = new System.Drawing.Point(143, 308);
+            this.btnGERAR.Name = "btnGERAR";
+            this.btnGERAR.Size = new System.Drawing.Size(203, 45);
+            this.btnGERAR.TabIndex = 0;
+            this.btnGERAR.Text = "GERAR";
+            this.btnGERAR.UseVisualStyleBackColor = true;
+            this.btnGERAR.Click += new System.EventHandler(this.btnGERAR_Click);
+            // 
+            // trkNIVEL
+            // 
+            this.trkNIVEL.Location = new System.Drawing.Point(455, 308);
+            this.trkNIVEL.Minimum = 1;
+            this.trkNIVEL.Name = "trkNIVEL";
+            this.trkNIVEL.Size = new System.Drawing.Size(203, 45);
+            this.trkNIVEL.TabIndex = 1;
+            this.trkNIVEL.Value = 5;
+            // 
+            // txtTEOREMA
+            // 
+            this.txtTEOREMA.BackColor = System.Drawing.SystemColors.Window;
+            this.txtTEOREMA.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTEOREMA.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTEOREMA.Location = new System.Drawing.Point(143, 96);
+            this.txtTEOREMA.Multiline = true;
+            this.txtTEOREMA.Name = "txtTEOREMA";
+            this.txtTEOREMA.Size = new System.Drawing.Size(515, 190);
+            this.txtTEOREMA.TabIndex = 2;
+            txtTEOREMA.KeyPress += new KeyPressEventHandler(txtTEOREMA_KeyPress);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTEOREMA);
+            this.Controls.Add(this.trkNIVEL);
+            this.Controls.Add(this.btnGERAR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trkNIVEL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGERAR;
+        private System.Windows.Forms.TrackBar trkNIVEL;
+        private System.Windows.Forms.TextBox txtTEOREMA;
+    }
+}
+
---
+++ tEOREMAdOmACACOiNFINITO/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace tEOREMAdOmACACOiNFINITO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGERAR_Click(object sender, EventArgs e)
+        {
+            int nivel = trkNIVEL.Value * 50;
+
+            string texto = Generatetexto(nivel);
+
+            txtTEOREMA.Text = texto;
+        }
+
+        static string Generatetexto(int nivel)
+        {
+            const string chars = "ABCDEFGHIJLMNOPQRSTUVXZ";
+
+            Random random = new Random();
+
+            char[] stringChars = new char[nivel];
+
+            for (int i = 0; i < nivel; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(stringChars);
+        }
+
+        private void txtTEOREMA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
